f17e873f1d322a4dd3a666e3fc580ba916ef07c3
dedouwe26 <63008025+dedouwe26@users.noreply.github.com>
2023-03-30 13:33:41 +0200

    Update BoxelRebound.js

--- BoxelRebound.js
+++ BoxelRebound.js
@@ -1,4 +1,4 @@
-// Link: https://chrome.google.com/webstore/detail/flappy-bird-offline-deskt/dlnkkghpoaboifilieokcpoclbhpoclo
+// Link: https://chrome.google.com/webstore/detail/boxel-rebound/iginnfkhmmfhlkagcmpgofnjhanpmklb
 
 // Please use fullscreen with Boxel Rebound.
 //        READ THESE MESSAGES!
