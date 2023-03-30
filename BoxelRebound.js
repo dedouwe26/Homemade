a7c36b67f0f84411e5ee9230b852c4a028e8c2bc
dedouwe26 <63008025+dedouwe26@users.noreply.github.com>
2023-03-30 12:27:30 +0200

    Create BoxelRebound.js

---
+++ BoxelRebound.js
@@ -0,0 +1,5 @@
+// Please use fullscreen with Boxel Rebound.
+//        READ THESE MESSAGES!
+// README: Press F while you are in a level.
+
+window.addEventListener('keydown', (e)=>{if (e.key==='f'){window.Game.player.completeLevel()}});
